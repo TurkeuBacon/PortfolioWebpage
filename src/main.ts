539383ce7bf879aec276b3c9b513bf1b7fe0d2a4
TurkeuBacon <turkeubacon@gmail.com>
2024-09-19 02:23:58 -0700

    Testing some stuff

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
+import $ from 'jquery';
+
 console.log("Initiating Webpage");
+
+$(function() {
+    $(".sidenavwrapper").one("mouseenter", onNavEnter);
+    $(".sidenavwrapper").one("mouseleave", onNavExit);
+});
+
+function onNavEnter() {
+    $(".sidenavwrapper").children(".sidenav").animate({left:'0px'},750);
+    $(".sidenavwrapper").children(".sidenavtab").animate({left:'250px'},750, ()=>{
+        $(".sidenavwrapper").one("mouseenter", onNavEnter);
+    });
+}
+function onNavExit() {
+    $(".sidenavwrapper").children(".sidenav").animate({left:'-250px'},750);
+    $(".sidenavwrapper").children(".sidenavtab").animate({left:'0px'},750, ()=>{
+        $(".sidenavwrapper").one("mouseleave", onNavExit);
+    });
+}
