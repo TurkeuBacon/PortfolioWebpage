5855e408d01872b54c379f7e16e8a5204f8d943c
TurkeuBacon <turkeubacon@gmail.com>
2024-09-20 00:06:02 -0700

    Trying to fix menu icon

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,27 @@
 import $ from 'jquery';
 
+let BASE = [location.protocol, "//", location.host].join('');
+if(BASE == "https://turkeubacon.github.io") {
+    console.log("GITHUB");
+    BASE += "/PortfolioWebpage";
+} else {
+    console.log("LOCAL");
+}
+
 console.log("Initiating Webpage");
 
-$(function() {
+$(function(): void {
     $(".sidenav-wrapper").one("mouseenter", onNavEnter);
     $(".sidenav-wrapper").one("mouseleave", onNavExit);
 });
 
-function onNavEnter() {
+function onNavEnter(): void {
     $(".sidenav-wrapper").animate({left:'250px'},750, ()=>{
         $(".sidenav-wrapper").children(".sidenavtab").children("p").text("< NAVIGATION <");
         $(".sidenav-wrapper").one("mouseenter", onNavEnter);
     });
 }
-function onNavExit() {
+function onNavExit(): void {
     $(".sidenav-wrapper").animate({left:'0px'},750, ()=>{
         $(".sidenav-wrapper").children(".sidenavtab").children("p").text("> NAVIGATION >");
         $(".sidenav-wrapper").one("mouseleave", onNavExit);
